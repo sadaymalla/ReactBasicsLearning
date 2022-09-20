d4c525e87d513583a7dceffb153b1e2692beb399
saday <saday.malla@gmail.com>
2022-09-20 11:19:05 +0545

    states

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,46 @@ import React,{Component} from "react";
 
 class Header extends Component {
 
-    render(){
-    return(
-        <header >
-            <div className="logo">Logo</div>
-            <input/>
-        </header>
-    )
+    state = {
+        name:'saday',
+        title:'The keywords are:',
+        keywords:'',
+        count:0
     }
+
+    inputChangeHandler = (event) =>{
+            this.setState({
+                keywords: event.target.value
+            })
+    }
+
+    addOne(){
+        this.setState((state,props)=>({
+            count: state.count +1
+        }))
+    }
+
+    render(){
+
+        console.log(this.state)
+        
+        return(
+            <header>
+                <div className="logo">Logo</div>
+                <input
+                    onChange={this.inputChangeHandler}
+                />
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
+                <br/>
+                <div>{this.state.count}</div>
+                <button onClick={()=>this.addOne()}>Add One</button>
+            </header>
+        )
+        }
 };
 
-// let styles = {
-//     header : {
-//         background : "#03a9fa"
-//     },
-//     logo:{
-//         color:"#fff",
-//         fontFamily:'Anton',
-//         textAlign :'Center'
-//     }
-// }
+
+
 
 export default Header;
